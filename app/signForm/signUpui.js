c9adf7e2d8e864deb8aa4ffc43808cfb22782d90
nmpellecchia <nmpellecchia@users.noreply.github.com>
2020-11-25 16:05:52 -0300

    working sign up
    Now it throws an error if the data submited is incorrect

--- app/signForm/signUpui.js
+++ app/signForm/signUpui.js
@@ -1,19 +1,48 @@
 const form = document.querySelector('#sign-up');
-const username = document.querySelector('#username').value;
-const email = document.querySelector('#email').value;
-const pass = document.querySelector('#pass').value;
-const passConf = document.querySelector('#pass-conf').value;
-const termService = document.querySelector('#terms-service').value;
 
-/* If input selected is != conditions of [input.ClassName], alert the user */
+/* const username = document.querySelector('#username').value;
+const email = document.querySelector('#email').value;
+const pass = document.querySelector('#password').value;
+const passConf = document.querySelector('#pass-conf').value;
+const termService = document.querySelector('#terms-service').value; */
+
+/* Swtich statement, if type checkbox, submit or other */
 
 function handleInput(e) {
     e.preventDefault();
-    
-    console.log(`${username.value == test}`);
-};
+    const inputs = form.querySelectorAll('input');
+    const patterns = {
+        username: /^[a-z\d]+$/i,
+        password: /^[a-z\d]{8,20}$/i, //Need another to reconfirm that has at least a letter and a number
+        email: /^([\w-\.]+)@([a-z0-9-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/,
+        get passConf() {
+            const pass = form.querySelector('#password');
+            const conf = new RegExp(pass.value);
+            return conf;
+        }
+    };
 
-const test = /[a-z]/;
-console.log(`${test.test(e.target.value)}`)
+    inputs.forEach(input => {
+        /* console.log(input.type); */
+        /* console.log(patterns[input.id].test(input.value)); */
+        const err = document.querySelector(`.error.${input.id}`);
+        err.style.display = 'none';
+
+        switch (input.type) {
+            case 'submit':
+                break;
+            case 'checkbox':
+                if(!input.checked) {
+                    err.style.display = 'block';
+                }; 
+                break;
+            default:
+                if(!patterns[input.id].test(input.value)) {
+                    err.style.display = 'block';
+                };
+        };
+    });
+    
+};
 
 form.addEventListener('submit', (e) => handleInput(e));
